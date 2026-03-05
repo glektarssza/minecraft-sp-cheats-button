05eddc09f6c85117c798e44590dcdf8256e54345
G'lek Tarssza <glek@glektarssza.com>
2026-03-05 16:21:49 -0700

    Missed two classes somehow...

---
+++ common/src/main/java/com/glektarssza/sp_cheats_button/CommonClass.java
@@ -0,0 +1,15 @@
+package com.glektarssza.sp_cheats_button;
+
+import com.glektarssza.sp_cheats_button.platform.Services;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Items;
+
+public class CommonClass {
+    public static void init() {
+        Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
+        Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
+        if (Services.PLATFORM.isModLoaded(Constants.MOD_ID)) {
+            Constants.LOG.info("Hello to {}", Constants.MOD_ID);
+        }
+    }
+}
---
+++ common/src/main/java/com/glektarssza/sp_cheats_button/Constants.java
@@ -0,0 +1,10 @@
+package com.glektarssza.sp_cheats_button;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String MOD_ID = "sp_cheats_button";
+	public static final String MOD_NAME = "Singleplayer Cheats Button";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
